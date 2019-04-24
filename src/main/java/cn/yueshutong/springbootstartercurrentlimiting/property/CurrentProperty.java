b9087f44e9f10edc6bc0b6416f317cb4fe565c03
onblog <codeblog@qq.com>
2019-04-24 13:39:26 +0800

    0.0.3.RELEASE：可自定义规则限流、增加令牌桶算法与漏桶算法的切换，纳秒级并发控制。

--- src/main/java/cn/yueshutong/springbootstartercurrentlimiting/property/CurrentProperty.java
+++ src/main/java/cn/yueshutong/springbootstartercurrentlimiting/property/CurrentProperty.java
@@ -11,7 +11,7 @@ public class CurrentProperty {
     /**
      * 自定义限流规则
      * @param id 标识名，若为IP地址则为IP地址限流，若为用户名则为用户限流，若为访问的URL则为接口限流。
-     * @param qps 每秒并发量，比如1分种允许100次调用，那么可以写：100/60。为0则禁止访问。
+     * @param qps 每秒并发量，支持小数、分数，计算规则：次数/时间(秒)。为0则禁止访问。
      * @param initialDelay 首次放入令牌延迟时间，可作为系统启动保护时间，毫秒。
      * @param failFast 是否需开启快速失败。
      * @param overflow 是否严格控制请求速率和次数。
