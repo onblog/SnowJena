7e9a77da77295516f43da7466c909a86dd2085a5
onblog <codeblog@qq.com>
2019-05-09 19:03:51 +0800

    整体重构

--- src/main/java/cn/yueshutong/springbootstartercurrentlimiting/current/flag/MyCurrentInterceptor.java
+++
@@ -1,9 +0,0 @@
-package cn.yueshutong.springbootstartercurrentlimiting.current.flag;
-
-import org.springframework.web.servlet.HandlerInterceptor;
-
-/**
- * Create by yster@foxmail.com 2019/5/1 0001 14:29
- */
-public interface MyCurrentInterceptor extends HandlerInterceptor {
-}
